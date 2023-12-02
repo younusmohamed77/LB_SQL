2cd2054fbc6fae89d8b64b34f18b77ea1146eaba
younusmohamed77 <younus.mohamed77@gmail.com>
2023-12-02 17:56:33 +0400

    Second day

--- 001_Creation.sql
+++ 001_Creation.sql
@@ -15,6 +15,34 @@ PHONE_NO varchar(13),
 EMAIL varchar(30)
 );
 
+ALTER TABLE EMP ADD CONSTRAINT NAME_CHCK CHECK(NAME IS NOT NULL);
+ALTER TABLE EMP ADD CONSTRAINT EID_CHCK CHECK(EID LIKE 'E%');
+
 -- Insert 10 appropriate records in the EMP table.
+select *
+from EMP;
+
+INSERT INTO EMP (EID, NAME, ADDR, CITY, DOB, PHONE_NO,	EMAIL)
+VALUES ('E001', 'Kaleemullah Syed', 'Elango Nagar, Veerapandi Pirivu', 'Coimbatore', '21-May-1985', '9876543210','kaleemullah.syed@imag.com');
+
+UPDATE EMP
+SET EID = 'E0001'
+WHERE EID = 'E001';
+
+INSERT INTO EMP (EID, NAME, ADDR, CITY, DOB, PHONE_NO,	EMAIL)
+VALUES
+	('E0002', 'Syed Mohamed Ali', 'Bogi Street', 'Wellington', '10-Oct-1979', '8765432190', 'syed.mohamed@imag.com'),
+	('E0003', 'Yasif Mohamed', 'Mel Bogi Street', 'Wellington', '14-	May-1991', '8976543210', 'yasif.mohamed@imag.com'),
+	('E0004', 'Mohamed Sherrif', 'Elango Nagar, Veerapandi Pirivu', 'Coimbatore', '01-Nov-1990', '9765432108', 'sherrif.mohamed@imag.ae'),
+	('E0005', 'Ismail Ameer', 'Mount Road, Connoor', 'Connoor', '14-Mar-1991', '9082714653', 'ismail.ameer@imag.in'),
+	('E0006', 'Sultan Syed', 'S.M. Nagar', 'Metupalayam','21-Nov-1984','8071936542','sultan.syed@imag.ae'),
+	('E0007', 'Mohamed Younus', 'Pudhunagar, Kandal', 'Ooty', '07-Dec-1995', '8072848392', 'younus.mohamed@imag.com'),
+	('E0008', 'Mohamed Irfan', 'Elango Nagar, Veerapandi Pirivu', 'Coimbatore', '01-Aug-1996', '9944509462', 'irfan.mohamed@imag.ae'),
+	('E0009', 'Sikanthar Suber Ali', 'Pollachi Rasakkapalayam', 'Pollachi', '21-May-1999', '9638527410', 'sikanthar.ali@imag.in'),
+	('E0010', 'Shameer Ahmed', 'Pudhunagar, Kandal', 'Ooty', '10-Mar-1999', '8489259842', 'shameer.ahmed@imag.in');
+
+ALTER TABLE EMP ALTER Column City varchar(15);
+
+-- Use SELECT command to view the contents of EMP table
 select *
 from EMP;
---
+++ 002_Updations.sql
@@ -0,0 +1,41 @@
+use DEMO;
+
+-- In the DEMO database create table EMP_SAL with the following fields
+	-- EID DEPT DESI DOJ SALARY
+create table EMP_SAL (
+	EID char(5),
+	DEPT varchar(15),
+	DESI varchar(10),
+	DOJ date,
+	SALARY int)
+
+select *
+from EMP_SAL;
+
+-- Insert 7 appropriate records in the EMP_SAL table
+
+insert INTO EMP_SAL (EID, DEPT, DESI, DOJ, SALARY)
+values 
+	('E0001', 'Security', 'Manager', '01-AUG-2008', '15000'),
+	('E0002', 'FM SUS', 'GenManager', '14-May-2000', '55000'),
+	('E0003', 'FM SUS', 'Admin', '21-Mar-2015', '7000'),
+	('E0004', 'Marketing', 'Officer', '01-Jan-2022', '6000'),
+	('E0005', 'Ops', 'DC','07-Jun-2017', '12000'),
+	('E0008', 'HR', 'DC', '12-Jan-2022', '10000'),
+	('E0009', 'Services','Manager', '10-Jan-2020', '10000');
+
+-- Use SELECT command to view the contents of EMP_SAL table
+select *
+from EMP_SAL;
+
+-- From EMP table list all employees with last name as Syed
+select *
+from EMP
+where NAME like '%Syed%'; 
+
+-- Increase the salary of all managers by 10%
+update EMP_SAL set SALARY = SALARY + 0.1*SALARY
+WHERE DESI like '%Manager%';
+
+select *
+from EMP_SAL;
